2bbb6330dfb89250dff40b027620f17b80d92104
Riya Sinha <33243383+riyavsinha@users.noreply.github.com>
2023-03-12 21:11:30 +0000

    feat: add complex kge example by generalizing transe example

--- torch_geometric/nn/kge/complex.py
+++ torch_geometric/nn/kge/complex.py
@@ -16,6 +16,13 @@ class ComplEx(KGEModel):
 
     .. math::
         d(h, r, t) = Re(< \mathbf{e}_h,  \mathbf{w}_r, \mathbf{e}_t>)
+        
+    .. note::
+
+        For an example of using the :class:`ComplEx` model, see
+        `examples/kge_fb15k_237.py
+        <https://github.com/pyg-team/pytorch_geometric/blob/master/examples/
+        kge_fb15k_237.py>`_.
 
     Args:
         num_nodes (int): The number of nodes/entities in the graph.
